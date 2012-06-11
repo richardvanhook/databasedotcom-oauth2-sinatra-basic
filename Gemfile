4a7387f6826e4c4b5d843bb593c86cda6a67a4b9
Richard Vanhook <richard.vanhook@gmail.com>
2012-06-11 16:58:07 -0500

    initial import

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source :rubygems
+gem "thin"
+gem "rack-ssl"
+gem "sinatra"
+gem "databasedotcom-oauth2", ">= 0.1.0"
+
+group :development do
+  gem "wirble"
+  gem "heroku"
+end
---
+++ app.rb
@@ -0,0 +1,35 @@
+require "sinatra/base"
+require "rack/ssl" unless ENV['RACK_ENV'] == "development"
+require "base64"
+require "databasedotcom-oauth2"
+
+class SinatraBasic < Sinatra::Base
+
+  use Rack::SSL unless ENV['RACK_ENV'] == "development"
+  use Rack::Session::Cookie
+  use Databasedotcom::OAuth2::WebServerFlow, 
+    :token_encryption_key => Base64.strict_decode64(ENV['TOKEN_ENCRYPTION_KEY']),
+    :endpoints            => {"login.salesforce.com" => {:key => ENV['CLIENT_ID'], :secret => ENV['CLIENT_SECRET']}},
+    :display              => "touch"
+    
+  get '/logout' do
+    request.env['rack.session'] = {}
+    redirect to("/")
+  end
+
+  get '/auth/salesforce/failure' do
+    "<html><body>Ruh-roh: #{params['message']}</body></html>"
+  end
+  
+  get '/*' do
+    if env['databasedotcom.client'].nil?
+      "<html><body>You're not logged in.  Click <a href=\"/auth/salesforce\">here</a> to login.</body></html>"
+    else
+      token = env['databasedotcom.token']
+      userinfo = nil
+      userinfo = token.post(token['id']).parsed unless token.nil?
+      "<html><body>You're logged in as #{userinfo['username']}.  Click <a href=\"/logout\">here</a> to logout.</body></html>"
+    end
+  end
+
+end
